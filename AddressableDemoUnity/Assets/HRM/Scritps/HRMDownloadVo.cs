7aa5a11d20c155e1eaa9a3086cf54cf5068cd38b
Danny <381558466@qq.com>
2022-06-10 20:33:22 +0800

    很迷

---
+++ AddressableDemoUnity/Assets/HRM/Scritps/HRMDownloadVo.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace HRM
+{
+    public class HRMDownloadVo
+    {
+        public AssetReference asset;
+
+        public int index;
+
+        public float loadProgress;
+    }
+}
